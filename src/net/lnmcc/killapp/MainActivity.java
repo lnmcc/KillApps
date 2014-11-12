a846e594a2d131b3f42536fbe17b41baeb39decc
sijiewang <wangsj0527@thundersoft.com>
2014-11-12 11:36:11 +0800

    ...

--- src/net/lnmcc/killapp/MainActivity.java
+++ src/net/lnmcc/killapp/MainActivity.java
@@ -7,7 +7,6 @@ import java.util.List;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
-import android.app.ActivityManager.RunningTaskInfo;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,141 +19,122 @@ import android.widget.SimpleAdapter;
 
 public class MainActivity extends Activity {
 
-	private static final String TAG = "KILL_APP_DEMO";
-	private static final int MSG_DATASET_CHANGED = 0;
+    private static final String TAG = "KILL_APP_DEMO";
+    private static final int MSG_DATASET_CHANGED = 0;
 
-	Button get_running_apps;
-	Button kill_all_apps;
-	ListView running_apps_lv;
+    private Button get_running_apps;
+    private Button kill_all_apps;
+    private ListView running_apps_lv;
 
-	ActivityManager am;
-	// List<RunningTaskInfo> runningTaskInfos = null;
-	List<RunningAppProcessInfo> runningAppProcessInfos = null;
-	List<String> whiteList = new ArrayList<String>();
+    private List<RunningAppProcessInfo> runningAppProcessInfos = null;
 
-	SimpleAdapter running_apps_adapter = null;
-	ArrayList<HashMap<String, String>> pkgNames = new ArrayList<HashMap<String, String>>();
+    private SimpleAdapter running_apps_adapter = null;
+    private ArrayList<HashMap<String, String>> pkgNames = new ArrayList<HashMap<String, String>>();
 
-	Object lock = new Object();
+    private ActivityManager am;
+    private Object lock = new Object();
+    private Handler handler;
 
-	private Handler handler;
+    private static List<String> whiteList = new ArrayList<String>();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    static {
+        whiteList.add("net.lnmcc.killapp");
+        whiteList.add("com.thunderst.weather");
+    }
 
-		whiteList.add("net.lnmcc.killapp");
-		whiteList.add("com.thunderst.weather");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		handler = new Handler() {
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-				case MSG_DATASET_CHANGED:
-					running_apps_adapter.notifyDataSetChanged();
-					break;
-				default:
-					break;
-				}
-			};
-		};
+        handler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                case MSG_DATASET_CHANGED:
+                    running_apps_adapter.notifyDataSetChanged();
+                    break;
+                default:
+                    break;
+                }
+            };
+        };
 
-		am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 
-		get_running_apps = (Button) findViewById(R.id.get_running_apps_btn);
-		get_running_apps.setOnClickListener(new View.OnClickListener() {
+        get_running_apps = (Button) findViewById(R.id.get_running_apps_btn);
+        get_running_apps.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				new Thread(new Runnable() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
 
-					@Override
-					public void run() {
-						getApps();
-						handler.sendEmptyMessage(MSG_DATASET_CHANGED);
-					}
-				}).start();
-			}
-		});
+                    @Override
+                    public void run() {
+                        getApps();
+                        handler.sendEmptyMessage(MSG_DATASET_CHANGED);
+                    }
+                }).start();
+            }
+        });
 
-		running_apps_adapter = new SimpleAdapter(this, pkgNames,
-				android.R.layout.simple_list_item_1,
-				new String[] { "pkgName" }, new int[] { android.R.id.text1 });
-		running_apps_lv = (ListView) findViewById(R.id.running_apps_lv);
-		running_apps_lv.setAdapter(running_apps_adapter);
+        running_apps_adapter = new SimpleAdapter(this, pkgNames,
+                android.R.layout.simple_list_item_1,
+                new String[] { "pkgName" }, new int[] { android.R.id.text1 });
 
-		kill_all_apps = (Button) findViewById(R.id.kill_all_apps_btn);
-		kill_all_apps.setOnClickListener(new View.OnClickListener() {
+        running_apps_lv = (ListView) findViewById(R.id.running_apps_lv);
+        running_apps_lv.setAdapter(running_apps_adapter);
 
-			@Override
-			public void onClick(View v) {
-				new Thread(new Runnable() {
+        kill_all_apps = (Button) findViewById(R.id.kill_all_apps_btn);
+        kill_all_apps.setOnClickListener(new View.OnClickListener() {
 
-					@Override
-					public void run() {
-						killApps();
-						handler.sendEmptyMessage(MSG_DATASET_CHANGED);
-					}
-				}).start();
-			}
-		});
-	}
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
 
-	private void killApps() {
-		synchronized (lock) {
-			if (runningAppProcessInfos == null
-					|| runningAppProcessInfos.size() == 0)
-				return;
-			/*
-			 * for (RunningTaskInfo info : runningTaskInfos) { if
-			 * (!info.baseActivity.getPackageName().equals(getPackageName())) {
-			 * Log.d(TAG, "Will kill " + info.baseActivity.getPackageName());
-			 * am.killBackgroundProcesses(info.baseActivity.getPackageName()); }
-			 * }
-			 */
-			for (RunningAppProcessInfo info : runningAppProcessInfos) {
-				if (/* info.pid != android.os.Process.myPid() */!whiteList
-						.contains(info.processName)) {
-					Log.d(TAG, "Will kill " + info.processName);
-					android.os.Process.killProcess(info.pid);
-					am.killBackgroundProcesses(info.processName);
-				}
-			}
-		}
-		getApps();
-	}
+                    @Override
+                    public void run() {
+                        killApps();
+                        getApps();
+                        handler.sendEmptyMessage(MSG_DATASET_CHANGED);
+                    }
+                }).start();
+            }
+        });
+    }
 
-	private void getApps() {
-		synchronized (lock) {
-			if (runningAppProcessInfos != null)
-				runningAppProcessInfos.clear();
+    private void killApps() {
+        synchronized (lock) {
+            if (runningAppProcessInfos == null
+                    || runningAppProcessInfos.size() == 0)
+                return;
+            for (RunningAppProcessInfo info : runningAppProcessInfos) {
+                if (!whiteList.contains(info.processName)) {
+                    Log.d(TAG, "Will kill " + info.processName);
+                    am.killBackgroundProcesses(info.processName);
+                }
+            }
+        }
+    }
 
-			/*
-			 * if (runningTaskInfos != null) runningTaskInfos.clear();
-			 */
+    private void getApps() {
+        synchronized (lock) {
+            if (runningAppProcessInfos != null)
+                runningAppProcessInfos.clear();
 
-			pkgNames.clear();
+            pkgNames.clear();
 
-			try {
-				runningAppProcessInfos = am.getRunningAppProcesses();
-				for (RunningAppProcessInfo info : runningAppProcessInfos) {
-					HashMap<String, String> map = new HashMap<String, String>();
-					map.put("pkgName", info.processName);
-					pkgNames.add(map);
-				}
-				/*
-				 * runningTaskInfos = am.getRunningTasks(1000); for
-				 * (RunningTaskInfo info : runningTaskInfos) { HashMap<String,
-				 * String> map = new HashMap<String, String>();
-				 * map.put("pkgName", info.baseActivity.getPackageName()); //
-				 * pkgNames.add(map); }
-				 */
+            try {
+                runningAppProcessInfos = am.getRunningAppProcesses();
+                for (RunningAppProcessInfo info : runningAppProcessInfos) {
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put("pkgName", info.processName);
+                    pkgNames.add(map);
+                }
 
-			} catch (SecurityException ex) {
-				Log.d(TAG, "Permission ERROR !");
-				// runningTaskInfos = null;
-				runningAppProcessInfos = null;
-			}
-		}
-	}
+            } catch (SecurityException ex) {
+                Log.d(TAG, "Permission ERROR !");
+                runningAppProcessInfos = null;
+            }
+        }
+    }
 }
